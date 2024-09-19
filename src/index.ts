23060d17a7498ea4d53eb77058e9781e02353620
Jonas Mattes <github@jmattes.de>
2024-09-19 19:37:15 +0200

    Add missing types and use generic export for all files.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { TransmissionPacket } from "./TransmissionPacket";
-export { TransmissionPacketFactory } from "./TransmissionPacketFactory";
-export { TagParser } from "./TagParser";
-export {WriteTextFileCommand} from './commands/TextFile/WriteTextFileCommand';
-export { SignClient} from './SignClient';
-export {text, html } from './elements';
+export * from "./TransmissionPacket";
+export * from "./TransmissionPacketFactory";
+export * from "./TagParser";
+export * from './commands/TextFile/WriteTextFileCommand';
+export * from './SignClient';
+export * from './elements';
 export * from './types';
 // TODO: Add missing exports
